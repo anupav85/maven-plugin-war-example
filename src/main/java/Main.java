639137392491ff48ee0fdad1f4f39a3f50bcba96
pavanG <anupav85@gmail.com>
2016-12-16 01:01:45 +0530

    first commit with change msg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,22 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
+/**
+ * @(#) $Id: eRTJalopyCodeConventions.xml 1657 2007-06-12 14:55:27Z worourke $
+ * Copyright (c) 2010 eResearchTechnology, Inc. All Rights Reserved.
+ */
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author skota Created On 16-Dec-2016
+ */
 public class Main extends HttpServlet {
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
-  }
+    @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java with Jenkins and GIT!");
+    }
 }
